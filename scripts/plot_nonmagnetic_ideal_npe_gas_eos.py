37379e289f15843cc16f4f7c2103388a27de454a
kkin1995 <7694983+kkin1995@users.noreply.github.com>
2025-01-20 04:04:37 +0000

    Separated calculation of EOS into three different regimes - proton-rich, neutron appearence, and neutron-rich. Also, wrote a Python script to plot the EOS

--- scripts/plot_nonmagnetic_ideal_npe_gas_eos.py
+++ scripts/plot_nonmagnetic_ideal_npe_gas_eos.py
@@ -1,18 +1,40 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data
 data = pd.read_csv("/mnt/c/Users/karan/Dropbox/KARAN/2 Areas/Education/PhD/3 Research/pulsarmhd/ideal_non_magnetic_npe_gas_eos.csv")
 log_rho = data['log_rho']
 log_P = data['log_P']
+log_n = data['log_n']
+converged = data['converged']
 
-# Plot
+Gamma = np.gradient(log_P, log_n)
+
+# Plot log_P vs log_rho
 plt.figure(figsize=(8, 6))
-plt.scatter(log_rho, log_P, label="B = 0 (nonmagnetic)", color='black', s = 5)
-
-# Labels and title
+plt.scatter(log_rho, log_P, label="B = 0 (nonmagnetic)", color='black', s=5)
 plt.xlabel(r'$\log(\rho)$ (g/cm$^3$)')
 plt.ylabel(r'$\log(P)$ (dyn/cm$^2$)')
 plt.grid(True)
 plt.legend()
 plt.savefig("ideal_non_magnetic_npe_gas_eos.png")
+
+# # Plot Gamma vs log_rho
+# plt.figure(figsize=(8, 6))
+# plt.scatter(log_rho, Gamma, label=r'$\Gamma$', color='blue', s=5)
+# plt.xlabel(r'$\log(\rho)$ (g/cm$^3$)')
+# plt.ylabel(r'$\Gamma$')
+# plt.grid(True)
+# plt.legend()
+# plt.savefig("gamma_vs_log_rho.png")
+
+plt.figure(figsize=(8, 6))
+plt.scatter(log_rho[converged == 1], Gamma[converged == 1], c='green', label='Converged', s=10)
+plt.scatter(log_rho[converged == 0], Gamma[converged == 0], c='red', label='Failed', s=10)
+plt.xlabel(r'$\log_{10}(\rho)$ (g/cm$^3$)')
+plt.ylabel(r'$\Gamma$')
+plt.legend()
+plt.grid(True)
+plt.title('Adiabatic Index')
+plt.savefig("gamma_vs_log_rho.png")
--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,10 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <iomanip>
+#include <algorithm>
 
 #define DEBUG 1
-
-// struct parameters {
-//     double m_e;
-//     double m_p;
-//     double m_n;
-//     double lambda_e;
-//     double lambda_p;
-//     double lambda_n;
-//     double n_B;
-// };
 
 struct parameters {
     double m_e;
@@ -25,12 +17,6 @@ struct parameters {
     double x_p;
     double x_e;
 };
-
-double Y_p(double n_B) {
-    double n_B_threshold = 7.37e29; // Approximate neutron appearance density
-    return 0.5 / (1 + exp((log10(n_B) - log10(n_B_threshold))));
-}
-
 
 double chi(double x) {
     double first_factor = 1 / (8.0 * pow(M_PI, 2.0));
@@ -65,34 +51,16 @@ void f_and_df(double x_n, void* p, double* f_value, double* df_value) {
     *df_value = f_prime(x_n, p);
 }
 
-
-// double f(double x, void* p) {
-//     struct parameters* params = (struct parameters*) p;
-//     double first_term = params->m_e * pow(1.0 + pow(x, 2.0) * ( pow(params->lambda_e, 2.0) / pow(params->lambda_p, 2.0) ), 1.0/2.0);
-//     double second_term = params->m_p * pow(1.0 + pow(x, 2.0), 1.0 / 2.0);
-//     double third_term;
-//     double n_n, x_n;
-
-//     if (params->n_B <= 7.37e30) {
-//         third_term = params->m_n;
-//         n_n = 0.0;
-//     } else {
-//         double second_factor_in_third_term = pow((1.0 + pow(abs(pow(params->lambda_n, 3.0) * params->n_B - ( (pow(params->lambda_n, 3.0) * pow(x, 3.0)) / (pow(params->lambda_p, 3.0)) )), 2.0 / 3.0)), 1.0 / 2.0);
-//         third_term = params->m_n * second_factor_in_third_term;
-//     }
-
-//     double result = first_term + second_term - third_term;
-
-//     return result;
-// }
-
 int main() {
     double m_p = 1.6726e-24; // grams
     double m_n = 1.6750e-24; // grams
     double m_e = 9.1094e-28; // grams
     double hbar = 1.0546e-27; // ergs * seconds
     double c = 2.9979e10; // cm / s
+    double n_B_threshold = 7.37e29; // Approximate neutron appearance density
+    double x_n, x_e, x_p;
     int status;
+    bool converged;
 
     const gsl_root_fdfsolver_type* T = gsl_root_fdfsolver_newton;
     gsl_root_fdfsolver* s = gsl_root_fdfsolver_alloc(T);
@@ -107,12 +75,12 @@ int main() {
         std::cerr << "Error opening file: " << filename << std::endl;
     }
 
-    outfile << "log_n,log_P,log_rho\n";
+    outfile << "log_n,log_P,log_rho,converged\n";
 
     std::vector<double> n_B_list;
-    double n_B_min = 1.0e10; // Before neutron appearance
-    double n_B_max = 1.0e40; // Well into neutron dominance
-    int num_points = 200;    // Higher resolution
+    double n_B_min = 1.0e28; // Before neutron appearance
+    double n_B_max = 1.0e32; // Well into neutron dominance
+    int num_points = 100;    // Higher resolution
 
     // Logarithmic spacing for smoother transitions
     for (int i = 0; i < num_points; i++) {
@@ -120,84 +88,103 @@ int main() {
         n_B_list.push_back(n_B_i);
     }
 
-    for (int i = 0; i < n_B_list.size(); i++) {
-        // double n_B = 7.37e30; // baryons / cm^3 - Neutron Appearence Density
+    double prev_x_e = 0.1;
+    double prev_x_n = 0.1;
+
+    double n_B_lower = 0.8 * n_B_threshold;
+    double n_B_upper = 1.2 * n_B_threshold;
+
+    for (std::size_t i = 0; i < n_B_list.size(); i++) {
         double n_B = n_B_list[i];
-        // double Y_p_current = Y_p(n_B);  // Calculate Y_p for current n_B
-        // double n_e = Y_p_current * n_B;
-        double n_e = n_B / 2.0; // Initial guess: n_e ≈ n_B / 2
-        double x_e = pow(((3 * pow(M_PI, 2.0) * pow(hbar, 3.0)) / pow(m_e * c, 3.0)) * n_e, 1.0 / 3.0);
 
-        double x_p = x_e * pow(lambda_e / lambda_p, 1.0 / 3.0);
+        std::cout << "n_B: " << n_B << std::endl;
 
-        parameters params = {
-            .m_e = m_e,
-            .m_p = m_p,
-            .m_n = m_n,
-            .x_p = x_p,
-            .x_e = x_e
-        };
+        if (n_B < n_B_lower) {
+            x_n = 0.0;
+            x_p = pow((3.0 * pow(M_PI, 2.0) * pow(lambda_p, 3.0) * n_B), 1.0 / 3.0);
+            x_e = x_p * lambda_e / lambda_p;
+            converged = 1;
+        } else if (n_B >= n_B_lower && n_B <= n_B_upper) {
+            // double S = smoothstep(n_B, n_B_lower, n_B_upper);
+            double excess = sqrt((n_B - n_B_threshold) / n_B_threshold); 
+            x_e = pow((3.0 * pow(M_PI, 2.0) * pow(lambda_e, 3.0) * n_B), 1.0 / 3.0);
+            x_p = x_e * lambda_p / lambda_e;
+            x_n = 0.01 * excess;
+            converged = 1;
+        } else {
+            x_e = prev_x_e;
+            x_n = pow((3.0 * pow(M_PI, 2.0) * pow(lambda_n, 3.0) * n_B), 1.0 / 3.0);
+            x_p = x_e * lambda_p / lambda_e;
 
-        gsl_function_fdf F;
-        F.f = &f;          // Function
-        F.df = &f_prime;   // Derivative
-        F.fdf = &f_and_df; // Combined
-        F.params = &params; // Parameters
+            parameters params = {
+                .m_e = m_e,
+                .m_p = m_p,
+                .m_n = m_n,
+                .x_p = x_p,
+                .x_e = x_e
+            };
 
-        // double x0 = pow(3.0 * pow(M_PI, 2.0) * pow(lambda_n, 3.0) * n_B, 1.0 / 3.0);
-        double x0 = 3.0;
+            gsl_function_fdf F;
+            F.f = &f;          // Function
+            F.df = &f_prime;   // Derivative
+            F.fdf = &f_and_df; // Combined
+            F.params = &params; // Parameters
 
-        gsl_root_fdfsolver_set(s, &F, x0);
-    
-        int iter = 0, max_iter = 200;
-        double x;
+            double x0 = x_n * 1.1;
 
-        do {
-            iter++;
-            status = gsl_root_fdfsolver_iterate(s);  // Perform an iteration
-            x = gsl_root_fdfsolver_root(s);         // Current root estimate
+            gsl_root_fdfsolver_set(s, &F, x0);
+        
+            int iter = 0, max_iter = 200;
+            double x;
 
-            // Convergence test based on the change in x
-            status = gsl_root_test_delta(x, x0, 0, 1e-6);
+            do {
+                iter++;
+                double f_value = f(x0, &params);
+                double f_prime_value = f_prime(x0, &params);
 
-            if (DEBUG) {
-                std::cout << "Iter " << iter << ": x = " << x << ", f(x) = " << f(x, &params) << std::endl;
+                status = gsl_root_fdfsolver_iterate(s);  // Perform an iteration
+                x = gsl_root_fdfsolver_root(s);         // Current root estimate
+
+                // Convergence test based on the change in x
+                status = gsl_root_test_delta(x, x0, 0, 1e-6);
+
+                if (DEBUG) {
+                    if (iter % 10 == 0) {
+                        std::cout << "Iter " << iter << ": x = " << x << ", f(x) = " << f(x, &params) << std::endl;
+                    }
+                }
+
+                x0 = x;  // Update the previous value
+
+            } while (status == GSL_CONTINUE && iter < max_iter);
+
+            if (status == GSL_SUCCESS) {
+                converged = true;
+            } else {
+                converged = false;
+                std::cerr << "Newton-Raphson failed for n_B = " << n_B << ". Skipping..." << std::endl;
             }
 
-            x0 = x;  // Update the previous value
+            if (status != GSL_CONTINUE && iter >= max_iter) {
+                std::cerr << "Exceeded max iterations. Skipping n_B = " << n_B << "." << std::endl;
+                break;
+            }
 
-        } while (status == GSL_CONTINUE && iter < max_iter);
+            x_n = x;
+            prev_x_e = x_e;
+            prev_x_n = x_n;
 
-        if (status != GSL_SUCCESS) {
-            std::cerr << "Newton-Raphson failed for n_B = " << n_B << ". Retrying with a different x0..." << std::endl;
-            x0 *= 0.5;  // Adjust initial guess and retry
-            gsl_root_fdfsolver_set(s, &F, x0);
         }
 
-        if (status != GSL_CONTINUE && iter >= max_iter) {
-            std::cerr << "Exceeded max iterations. Skipping n_B = " << n_B << "." << std::endl;
-            break;
-        }
-
-        double x_n = x;
-
-        // double x_e = lambda_e * (x_p / lambda_p);
-        // double x_n;
-        // if (n_B <= 7.37e30) {
-        //     x_n = 0;
-        // } else {
-        //     x_n = pow(abs(pow(lambda_n, 3.0) * (n_B - (pow(x_p, 3.0))/(pow(lambda_p, 3.0)))), 1.0 / 3.0);
-        // }
-
-        // double n_e = (1.0 / (3.0 * M_PI * pow(lambda_e, 3.0))) * pow(x_e, 3.0);
+        double n_e = (1.0 / (3.0 * pow(M_PI, 2.0) * pow(lambda_e, 3.0))) * pow(x_e, 3.0);
         double epsilon_e = ((m_e * pow(c, 2.0)) / (pow(lambda_e, 3.0))) * chi(x_e);
         double P_e = ((m_e * pow(c, 2.0)) / (pow(lambda_e, 3.0))) * phi(x_e);
 
-        double n_p = (1.0 / (3.0 * M_PI * pow(lambda_p, 3.0))) * pow(x_p, 3.0);
+        double n_p = (1.0 / (3.0 * pow(M_PI, 2.0) * pow(lambda_p, 3.0))) * pow(x_p, 3.0);
         double epsilon_p = ((m_p * pow(c, 2.0)) / (pow(lambda_p, 3.0))) * chi(x_p);
         double P_p = ((m_p * pow(c, 2.0)) / (pow(lambda_p, 3.0))) * phi(x_p);
 
-        double n_n = (1.0 / (3.0 * M_PI * pow(lambda_n, 3.0))) * pow(x_n, 3.0);
+        double n_n = (1.0 / (3.0 * pow(M_PI, 2.0) * pow(lambda_n, 3.0))) * pow(x_n, 3.0);
         double epsilon_n = ((m_n * pow(c, 2.0)) / (pow(lambda_n, 3.0))) * chi(x_n);
         double P_n = ((m_n * pow(c, 2.0)) / (pow(lambda_n, 3.0))) * phi(x_n);
 
@@ -205,23 +192,28 @@ int main() {
         double rho = (epsilon_e + epsilon_p + epsilon_n) / (pow(c, 2.0));
         double n = n_e + n_p + n_n;
 
-        std::cout << "n_e: " << n_e << std::endl;
-        std::cout << "epsilon_e: " << epsilon_e << std::endl;
-        std::cout << "P_e: " << P_e << std::endl;
+        if (P < 0 || rho < 0 || n < 0) {
+            std::cerr << "Warning: Negative values detected. n_B = " << n_B
+                    << ", P = " << P << ", rho = " << rho << ", n = " << n << std::endl;
+        }
 
-        std::cout << "n_p: " << n_p << std::endl;
-        std::cout << "epsilon_p: " << epsilon_p << std::endl;
-        std::cout << "P_p: " << P_p << std::endl;
+        // std::cout << "n_e: " << n_e << std::endl;
+        // std::cout << "epsilon_e: " << epsilon_e << std::endl;
+        // std::cout << "P_e: " << P_e << std::endl;
 
-        std::cout << "n_n: " << n_n << std::endl;
-        std::cout << "epsilon_n: " << epsilon_n << std::endl;
-        std::cout << "P_n: " << P_n << std::endl;
+        // std::cout << "n_p: " << n_p << std::endl;
+        // std::cout << "epsilon_p: " << epsilon_p << std::endl;
+        // std::cout << "P_p: " << P_p << std::endl;
+
+        // std::cout << "n_n: " << n_n << std::endl;
+        // std::cout << "epsilon_n: " << epsilon_n << std::endl;
+        // std::cout << "P_n: " << P_n << std::endl;
 
         std::cout << "P: " << P << std::endl;
         std::cout << "rho: " << rho << std::endl;
         std::cout << "n: " << n << std::endl;
 
-        outfile << log10(n) << "," << log10(P) << "," << log10(rho) << "\n";
+        outfile << std::setprecision(10) << log10(n) << "," << log10(P) << "," << log10(rho) << "," << static_cast<int>(converged) << "\n";
     }
 
     outfile.close();
